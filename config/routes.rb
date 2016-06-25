ebaf2c8f7dcda36bea9163af8ad33bf5f3c59325
ramywafa <ramy.wafa@gmail.com>
2016-06-25 22:50:29 +0200

    Routes modification and removing turbo-links completely from the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :dishes except: [:new, :edit] do
+      resources :dishes, except: [:new, :edit] do
         resources :reviews, only: [:create, :index, :update, :destroy]
       end
     end
   end
+
+  root to: "home#show"
 end
