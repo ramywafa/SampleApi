59ce992267c945d6e4e9c2e0e8f903ee63978c2d
ramywafa <ramy.wafa@gmail.com>
2016-06-26 04:13:12 +0200

    Added validation rules to the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -24,4 +24,8 @@
 class Review < ActiveRecord::Base
   belongs_to :dish
   belongs_to :reviewer, polymorphic: true
+
+  validates :dish, :reviewer, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :content, length: { minimum: 2 }
 end
