6f1e3af93cff2b4dc2744724fccf0cfe2793fa7a
ramywafa <ramy.wafa@gmail.com>
2016-06-27 18:37:11 +0200

    Added devise sessions and registration controllers and routes for using new custom controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      devise_for :admins, format: :json
-      devise_for :users, format: :json
+      devise_for :admins, format: :json,
+        controllers: { registrations: 'api/v1/admins/registrations',
+                       sessions: 'api/v1/sessions' }
+      devise_for :users, format: :json,
+        controllers: { registrations: 'api/v1/users/registrations',
+                       sessions: 'api/v1/sessions' }
       resources :dishes, except: [:new, :edit] do
         resources :reviews, only: [:create, :index, :update, :destroy]
       end
