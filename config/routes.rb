1512d143ab5ad3ebca22c01d90335c648a460b79
ramywafa <ramy.wafa@gmail.com>
2016-06-29 19:34:06 +0200

    Removed unnecessary session controller as authentication basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       devise_for :admins, format: :json,
-        controllers: { registrations: 'api/v1/admins/registrations',
-                       sessions: 'api/v1/sessions' }
+        controllers: { registrations: 'api/v1/admins/registrations' }
       devise_for :users, format: :json,
-        controllers: { registrations: 'api/v1/users/registrations',
-                       sessions: 'api/v1/sessions' }
+        controllers: { registrations: 'api/v1/users/registrations' }
       resources :admins, only: :show
       resources :users, only: :show
       resources :dishes, only: [:index, :create, :show, :update, :destroy] do
