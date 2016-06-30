a123a194fe912ac754eb562a53952f064e5aaa4a
ramywafa <ramy.wafa@gmail.com>
2016-06-30 02:58:24 +0200

    Added json presentations and reviews controller

---
+++ app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::ReviewsController < Api::BaseController
+  before_action :load_dish
+  before_action :load_review, only: [:show, :update, :destroy]
+  before_action :validate_current_client_is_admin_or_reviewer, only: [:update, :destroy]
+
+  def index
+    respond_with @reviews = @dish.reviews.page(page)
+  end
+
+  def create
+    @review = @dish.reviews.build review_params
+    @review.reviewer = current_client
+    @review.save
+    respond_with @review, status: :created
+  end
+
+  def show
+    respond_with @review
+  end
+
+  def update
+    respond_with @review.update(review_params)
+  end
+
+  def destroy
+    respond_with @review.destroy
+  end
+
+private
+  def load_dish
+    @dish = Dish.find params[:dish_id]
+  end
+
+  def load_review
+    @review = Review.find params[:id]
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+
+  def validate_current_client_is_admin_or_reviewer
+    if current_client.nil? || (current_api_user.present? && current_api_user != @review.reviewer)
+      fail Unauthorized
+    end
+  end
+end
