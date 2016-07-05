dfce9784ff4a411f5a6cdf4616aa656ef010207a
ramywafa <ramy.wafa@gmail.com>
2016-07-05 20:00:30 +0200

    Finalized views for index page to handle json responses and consume api

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,8 +2,9 @@ class DishesController < ApplicationController
   before_action :load_dish, only: [:show, :edit]
 
   def index
-    response = RestClient.get("http://localhost:3000/api/dishes")
-    @dishes = JSON.parse(response.body)['dishes']
+    page = params[:page] || 1
+    response = RestClient.get("http://localhost:3000/api/dishes?page=#{page}")
+    @dishes = Kaminari.paginate_array(JSON.parse(response.body)['dishes']).page(page)
   end
 
   def show
--- app/views/api/v1/dishes/base.rabl
+++ app/views/api/v1/dishes/base.rabl
@@ -1,1 +1,3 @@
 attributes :id, :created_at, :name, :description
+
+node(:reviews_count) { |dish| dish.reviews.count }
