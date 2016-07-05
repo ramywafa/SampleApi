281e1d65693b84b387ebe92896a0e86294bc9c0e
ramywafa <ramy.wafa@gmail.com>
2016-07-05 19:44:39 +0200

    Recieving api call from api name space using rest-client

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,7 +2,8 @@ class DishesController < ApplicationController
   before_action :load_dish, only: [:show, :edit]
 
   def index
-    @dishes = Dish.page(params[:page])
+    response = RestClient.get("http://localhost:3000/api/dishes")
+    @dishes = JSON.parse(response.body)['dishes']
   end
 
   def show
